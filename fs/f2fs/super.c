93e805aab4be303f0b689f87083c28ea55a13441
tarsin <yuanqingxiang233@163.com>
2024-06-15 12:22:31 +0300

    f2fs: Set age_extent_cache by default

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -2111,6 +2111,8 @@ static void default_options(struct f2fs_sb_info *sbi)
 
 	sbi->sb->s_flags &= ~SB_INLINECRYPT;
 
+
+	set_opt(sbi, AGE_EXTENT_CACHE);
 	set_opt(sbi, ATGC);
 	set_opt(sbi, GC_MERGE);
 	set_opt(sbi, INLINE_XATTR);
