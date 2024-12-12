kerneltoast/android_kernel_google_gs201

drm: samsung: Don't busy wait for CGC DMA completion
Busy waiting for CGC DMA completion isn't necessary since waiting is only
ever done from process context, thus there's no need to busy wait.

Use a sleepable readl poll to reduce overhead. Adjust the sleep duration to
4 us to make readl_poll_timeout() use a sleep range of 2 us, since it
divides the sleep duration in half, whereas readl_poll_timeout_atomic()
doesn't.

Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

google-modules/display/samsung/cal_9855/dqe_reg.c
@@ -34,9 +34,9 @@ int dqe_reg_wait_cgc_dma_done_internal(u32 id, unsigned long timeout_us)
 	u32 val;
 	int ret;
 
-	ret = readl_poll_timeout_atomic(dqe_regs_desc(id)->regs +
+	ret = readl_poll_timeout(dqe_regs_desc(id)->regs +
 			cgc_offset(regs_dqe[id].version) + DQE_CGC_CON, val,
-			!(val & CGC_COEF_DMA_REQ), 2, timeout_us);
+			!(val & CGC_COEF_DMA_REQ), 4, timeout_us);
 	if (ret) {
 		cal_log_err(id, "timeout of CGC COEF DMA request (0x%x)\n",
 				!(val & CGC_COEF_DMA_REQ));

murikbmx/android_kernel_google_zuma

google-modules/display/samsung/cal_9865/dqe_reg.c
@@ -887,9 +887,9 @@ int dqe_reg_wait_cgc_dma_done_internal(u32 dqe_id, unsigned long timeout_us)
 	u32 val;
 	int ret;
 
-	ret = readl_poll_timeout_atomic(dqe_cgc_regs_desc(dqe_id)->regs +
+	ret = readl_poll_timeout(dqe_cgc_regs_desc(dqe_id)->regs +
 			DQE_CGC_CON, val,
-			!(val & CGC_COEF_DMA_REQ), 2, timeout_us);
+			!(val & CGC_COEF_DMA_REQ), 4, timeout_us);
 	if (ret) {
 		cal_log_err(dqe_id, "timeout of CGC COEF DMA request (0x%x)\n",
 				!(val & CGC_COEF_DMA_REQ));